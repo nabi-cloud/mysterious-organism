fdd254301e00c8ba07d3a7489b86c2b26c37e681
Abigail Matibag <118436896+nabi-cloud@users.noreply.github.com>
2023-09-09 20:10:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)]
+}
+  
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+    const newStrand = []
+        for (let i = 0; i < 15; i++) {
+            newStrand.push(returnRandBase())
+        }
+    return newStrand
+}
+
+const pAequorFactory = (specimanNum, dna) => {
+    return {
+        specimanNum,
+        dna,
+
+        mutate() {
+            const randIndex = Math.floor(Math.random() * this.dna.length);
+            let newBase = returnRandBase();
+            while (this.dna[randIndex] === newBase) {
+                newBase = returnRandBase();
+            }
+            this.dna[randIndex] = newBase;
+            return this.dna;
+        },
+
+        compareDNA(otherOrganism) {
+            const similarities = this.dna.reduce((acc, curr, arr, indx) => {
+                if (arr[indx] === otherOrganism.dna[indx]) {
+                return acc++;
+                console.log(arr[idx])
+                console.log(otherOrg.dna[idx])
+                } else {
+                return acc;
+                }
+            }, 0);
+            const percentOfDNAshared = (similarities / this.dna.length) * 100;
+            const percentageTo2Deci = percentOfDNAshared.toFixed(2);
+            console.log(`${this.specimanNum} and ${otherOrganism.specimanNum} have ${percentageTo2Deci}% DNA in common`);
+            },
+
+            willLikelySurvive() {
+            const cOrG = this.dna.filter(element => element === "C" || element === "G")
+            const percentageOfSurvival = cOrG.length / this.dna.length;
+            if (percentageOfSurvival >= 0.60) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+};
+  
+const survivingSpecimen = [];
+let idCounter = 1;
+  
+while (survivingSpecimen.length < 30) {
+    let newOrg = pAequorFactory(idCounter, mockUpStrand());
+    if (newOrg.willLikelySurvive()) {
+        survivingSpecimen.push(newOrg);
+    }
+    idCounter++;
+}
+  
+console.log(survivingSpecimen);
